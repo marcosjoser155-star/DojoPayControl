c45f5b7c868edc1d52cc00712cf0b008aba89876
MARCOS RILEY <marcos.riley@utp.ac.pa>
2026-05-21 16:52:33 -0500

    Actualizar cambios: sincronizar workspace

---
+++ DojoPayControl/Clases/ConexipmDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DojoPayControl.Clases
+{
+    internal class ConexipmDB
+    {
+    }
+}
